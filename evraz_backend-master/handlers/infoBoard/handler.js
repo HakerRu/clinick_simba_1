e19bf9907e62f725716a207f4ad1442e0da9dc34
HakerRu <126074024+HakerRu@users.noreply.github.com>
2024-08-13 09:48:09 +0300

    asdf

---
+++ evraz_backend-master/handlers/infoBoard/handler.js
@@ -0,0 +1,146 @@
+const {pool} = require("../../dependencies");
+
+async function addInfo(object){
+    const funcName = 'addInfo';
+    const client = await pool.connect();
+    const data = {
+        message:    'error',    statusCode: 400,
+    };
+    try {
+        await client.query(`INSERT INTO info_board  ("firstName", "information", "img")
+                                VALUES ($1, $2,$3)`,
+            [
+                object.firstName,
+                object.information,
+                object.img
+            ]);
+        data.statusCode =200
+        data.message = 'all good'
+
+    }catch (err){
+        console.log(err.message, err.stack);
+    }
+
+    finally {
+        client.release();
+        console.log(`${ funcName }: client release()`);
+    }
+    return data;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+async function deleteInfo(object){
+    const funcName = 'deleteInfo';
+    const client = await pool.connect();
+    const data = {
+        message:    'error',    statusCode: 400,
+    };
+    try {
+
+        await client.query(`DELETE FROM info_board where "firstName" = $1`,[object.firstName])
+        data.statusCode =200
+        data.message = 'all good'
+
+    }catch (err){
+        console.log(err.message, err.stack);
+    }
+
+    finally {
+        client.release();
+        console.log(`${ funcName }: client release()`);
+    }
+    return data;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+async function updateInfo(object){
+    const funcName = 'updateInfo';
+    const client = await pool.connect();
+    const data = {
+        message:    'error',    statusCode: 400,
+    };
+    try {
+        const countChanger = object["countChanger"]
+        const changeName = object["changeName"]
+        //изменение загаловка
+        if (countChanger === 1){
+        await client.query('UPDATE info_board SET "firstName" = $1 where "firstName" = $2', [object.firstName, changeName ])
+            data.statusCode =200
+            data.message = 'all good'
+        }
+        //изменение текста
+        if (countChanger === 2){
+            await client.query('UPDATE info_board SET "information" = $1 where "firstName" = $2', [object.information, changeName])
+            data.statusCode =200
+            data.message = 'all good'
+        }
+        //изменение картинки
+        if (countChanger === 3){
+            await client.query('UPDATE info_board SET "img" = $1 where "firstName" = $2', [object.img, changeName])
+            data.statusCode =200
+            data.message = 'all good'
+        }
+
+
+    }catch (err){
+        console.log(err.message, err.stack);
+    }
+
+    finally {
+        client.release();
+        console.log(`${ funcName }: client release()`);
+    }
+    return data;
+
+
+}
+async function selectBoardInfo(object){
+    const funcName = 'selectBoardInfo';
+    const client = await pool.connect();
+    const data = {
+        message:    'error',    statusCode: 400, board: ''
+    };
+    try {
+        const selectBoardInfo = await client.query(`SELECT * FROM info_board`);
+        data.statusCode =200
+        data.message = 'all good'
+        data.board = selectBoardInfo.rows;
+    }catch (err){
+        console.log(err.message, err.stack);
+    }
+
+    finally {
+        client.release();
+        console.log(`${ funcName }: client release()`);
+    }
+    return data;
+
+
+}
+module.exports = {
+    addInfo:addInfo,
+    deleteInfo:deleteInfo,
+    updateInfo:updateInfo,
+    selectBoardInfo:selectBoardInfo
+}
